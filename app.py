a7fd86402db384cc656adc51c00e0fb4e335247a
Kamila Samolej <96179811+sawolej@users.noreply.github.com>
2024-11-30 03:25:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -338,5 +338,9 @@ def update_time_gating():
 
     return jsonify({"plot_url": f"/{plot_path}"})
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from environment or default to 5000
+    app.run(host="0.0.0.0", port=port)  # Bind to 0.0.0.0 to allow external connections
+
